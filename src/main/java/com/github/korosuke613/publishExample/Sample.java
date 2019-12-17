7c1c89b8f6e5c060e157a3d383ef099ada356eb6
Futa Hirakoba <korosuke613613@gmail.com>
2019-12-17 14:47:23 +0900

    create Sample class

---
+++ src/main/java/com/github/korosuke613/publishExample/Sample.java
@@ -0,0 +1,25 @@
+package com.github.korosuke613.publishExample;
+
+import java.util.ArrayList;
+
+public class Sample {
+    private ArrayList<String> calcLog = new ArrayList<>();
+
+    public int sum(int a, int b) {
+        var result = a + b;
+        calcLog.add(String.valueOf(result));
+
+        return result;
+    }
+
+    public String sum(String a, String b) {
+        var result = a + b;
+        calcLog.add(result);
+
+        return a + b;
+    }
+
+    public ArrayList<String> getCalcLog() {
+        return calcLog;
+    }
+}
